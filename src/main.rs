06bebe9ae784949e0bda3bdcac20dbc8d22a0af2
Henry Barreto <contato@henrybarreto.dev>
2021-06-26 16:53:50 -0300

    Created UI

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 //! GTK application what uses the 'wandershaper' script what uses some networking tools to limit the bandwidth and helping saving data.
 
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 mod wondershaper;
-use wondershaper::Wondershaper;
 
 fn main() {
-    let wondershaper = Wondershaper::new(String::from("config/wondershaper.conf"));
-    print!("{:#?}", wondershaper.wondershaper_config);
+    let builder = gtk::Builder::from_file(""); 
+    let app = Application::builder()
+        .application_id("dev.henrybarreto.bandwidth-saving")
+        .build();
 
+    app.connect_activate(|app| {
+        // We create the main window.
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        // Don't forget to make all widgets visible.
+        win.show_all();
+    });
+    app.run();
 }
