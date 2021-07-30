b36910d223921a93b5bcbc885a215738495ac39a
Henry Barreto <contato@henrybarreto.dev>
2021-07-29 23:44:57 -0300

    Imroved wondershaper config structure and use

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@
 
 use std::{process, rc::Rc};
 
-use gtk::prelude::*;
 use gio::{glib::GString, prelude::*};
+use gtk::prelude::*;
 
 use gtk::Application;
 use wondershaper::{Wondershaper, WondershaperConfig, WondershaperConfigFile};
@@ -14,64 +14,65 @@ fn main() {
     // let wondershaper: Wondershaper = Wondershaper::new("./config/wondershaper.conf".to_string());
     let interfaces: Rc<Vec<String>> = Rc::new(Wondershaper::get_network_interfaces_name());
     // let wondershaper_config = Wondershaper::create_cofiguration_file("./config/wondershaper.conf", wondershaper_config);
-    gtk::init().
-        expect("Could not init the gtk");
+    gtk::init().expect("Could not init the gtk");
     let app = Application::builder()
         .application_id("dev.henrybarreto.bandwidth-saving")
         .build();
 
-    let builder = gtk::Builder::from_file("./config/ui.glade"); 
+    let builder = gtk::Builder::from_file("./config/ui.glade");
 
     app.connect_activate(move |app| {
-        let win_main: gtk::Window = builder.object("win_main")
+        let win_main: gtk::Window = builder
+            .object("win_main")
             .expect("Could not find the win_main in the ui file");
-        let cbb_network_intefaces: gtk::ComboBoxText = builder.object("cbb_network_interfaces")
+        let cbb_network_intefaces: gtk::ComboBoxText = builder
+            .object("cbb_network_interfaces")
             .expect("Could not find the cbb_network_interfaces in the ui file");
-        let btn_apply: gtk::Button = builder.object("btn_apply")
+        let btn_apply: gtk::Button = builder
+            .object("btn_apply")
             .expect("Could not find the btn_apply in the ui file");
-        let et_downlaod_speed: gtk::Entry = builder.object("et_downlaod_speed")
+        let et_downlaod_speed: gtk::Entry = builder
+            .object("et_downlaod_speed")
             .expect("Could not find the et_download_speed in the ui file");
-        let et_upload_speed: gtk::Entry = builder.object("et_upload_speed")
+        let et_upload_speed: gtk::Entry = builder
+            .object("et_upload_speed")
             .expect("Could not find the et_upload_speed in the ui file");
         // ---
-        let insert_data_combo_box_text = |combo_box_text: &gtk::ComboBoxText, vec_data: Vec<String>| {
-            vec_data
-            .iter()
-            .enumerate().
-            inspect(|(index, data)| {
-                combo_box_text.insert_text(*index as i32, *data);
-            }).map(|(_, data)| {data})
-            .for_each(drop);
-        };
+        let insert_data_combo_box_text =
+            |combo_box_text: &gtk::ComboBoxText, vec_data: Vec<String>| {
+                vec_data
+                    .iter()
+                    .enumerate()
+                    .inspect(|(index, data)| {
+                        combo_box_text.insert_text(*index as i32, *data);
+                    })
+                    .map(|(_, data)| data)
+                    .for_each(drop);
+            };
         let capture_combo_box_text_data = |combo_box_text: &gtk::ComboBoxText| {
-            combo_box_text.active_text().unwrap_or(GString::from("")).to_string()
+            combo_box_text
+                .active_text()
+                .unwrap_or(GString::from(""))
+                .to_string()
         };
 
-        let capture_entry_data = |entry: &gtk::Entry| {
-            entry.text().to_string()
-        };
-           
+        let capture_entry_data = |entry: &gtk::Entry| entry.text().to_string();
+
         let cbb_network_intefaces_clone = cbb_network_intefaces.clone();
         btn_apply.connect_button_press_event(move |_btn, _event| {
             let interface_selected = capture_combo_box_text_data(&cbb_network_intefaces_clone);
             let download_speed = capture_entry_data(&et_downlaod_speed);
             let upload_speed = capture_entry_data(&et_upload_speed);
             // let wondershaper = Wondershaper::new("./config/wondershaper.conf".to_string());
-            let wondershaper_config = WondershaperConfig {
-                IFACE: interface_selected,
-                DSPEED: download_speed,
-                USPEED: upload_speed,
-                HIPRIODST: Option::None,
-                COMMONOPTIONS: Option::None,
-                NOPRIOHOSTSRC: Option::None,
-                NOPRIOHOSTDST: Option::None,
-                NOPRIOPORTSRC: Option::None,
-                NOPRIOPORTDST: Option::None
-            };
+            let wondershaper_config =
+                WondershaperConfig::new(interface_selected, download_speed, upload_speed);
             let wondershaper_config_file = WondershaperConfigFile {
-                wondershaper: wondershaper_config
+                wondershaper: wondershaper_config,
             };
-            Wondershaper::create_cofiguration_file("./config/wondershaper.conf".to_string(), &wondershaper_config_file);
+            Wondershaper::create_cofiguration_file(
+                "./config/wondershaper.conf".to_string(),
+                &wondershaper_config_file,
+            );
 
             process::exit(0);
             Inhibit(false)
--- src/wondershaper.rs
+++ src/wondershaper.rs
@@ -1,66 +1,75 @@
-use pnet::datalink::{NetworkInterface, interfaces};
-use serde::{Serialize, Deserialize};
+use pnet::datalink::{interfaces, NetworkInterface};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
 use std::fs::File;
 use std::io::{Read, Write};
-use std::error::Error;
 
 /**
   Structure with the main methods to perfom action in the wondershaper script
 */
 pub struct Wondershaper {
-  pub wondershaper_config: WondershaperConfig
+    pub wondershaper_config: WondershaperConfig,
 }
 
 impl Wondershaper {
-  pub fn new(configuration_file_path: String) -> Self {
-    Wondershaper {
-      wondershaper_config: Wondershaper::load_configuration_file(configuration_file_path)
+    pub fn new(configuration_file_path: String) -> Self {
+        Wondershaper {
+            wondershaper_config: Wondershaper::load_configuration_file(configuration_file_path),
+        }
     }
-  }
-  pub fn get_network_interfaces_name() -> Vec<String> {
-    interfaces().iter()
-    .map(|interface| interface.name.to_owned())
-    .collect::<Vec<String>>()
-  }
+    pub fn get_network_interfaces_name() -> Vec<String> {
+        interfaces()
+            .iter()
+            .map(|interface| interface.name.to_owned())
+            .collect::<Vec<String>>()
+    }
 
-  fn bandwidth_save(inteface: String, download_speed: String, upload_speed: String) -> Result<(), Box<dyn Error>> {
-    // TODO Fazer a implementção principal da aplicação
-    Ok(())
-  }
+    fn bandwidth_save(
+        inteface: String,
+        download_speed: String,
+        upload_speed: String,
+    ) -> Result<(), Box<dyn Error>> {
+        // TODO Fazer a implementção principal da aplicação
+        Ok(())
+    }
 
-  /**
-    Loading a wondershaper.conf file from a path and return a WondershaperConfig.
+    /**
+      Loading a wondershaper.conf file from a path and return a WondershaperConfig.
 
-    If the file could not be open, the application will panic.
-  */
-  fn load_configuration_file(configuration_file_path: String) -> WondershaperConfig {
-    let mut wondershaper_file: File = File::open(configuration_file_path)
-      .expect("Could not find the wondershaper.conf file");
-    let mut data_from_wondershaper_file: Vec<u8> = vec![];
-    let _bytes_read_from_wondershaper_file: usize = wondershaper_file
-      .read_to_end(&mut data_from_wondershaper_file).expect("Could not read the wondershaper.conf file");
-    let config_from_wondershaper_file = toml::from_slice::<WondershaperConfigFile>(&mut data_from_wondershaper_file)
-      .expect("Could not deserialize the wondershaper.conf file");
-    config_from_wondershaper_file.wondershaper
-  }
-  /**
-    Creating a wondershaper.conf file to a path and return a Vec<u8> with the bytes wrote..
+      If the file could not be open, the application will panic.
+    */
+    fn load_configuration_file(configuration_file_path: String) -> WondershaperConfig {
+        let mut wondershaper_file: File =
+            File::open(configuration_file_path).expect("Could not find the wondershaper.conf file");
+        let mut data_from_wondershaper_file: Vec<u8> = vec![];
+        let _bytes_read_from_wondershaper_file: usize = wondershaper_file
+            .read_to_end(&mut data_from_wondershaper_file)
+            .expect("Could not read the wondershaper.conf file");
+        let config_from_wondershaper_file =
+            toml::from_slice::<WondershaperConfigFile>(&mut data_from_wondershaper_file)
+                .expect("Could not deserialize the wondershaper.conf file");
+        config_from_wondershaper_file.wondershaper
+    }
+    /**
+      Creating a wondershaper.conf file to a path and return a Vec<u8> with the bytes wrote..
 
-    If the file could not be created, the application will panic.
-  */
-  pub fn create_cofiguration_file(configuration_file_path: String, wondershaper_config: &WondershaperConfigFile) -> Vec<u8>{
-    let mut wondershaper_file = File::create(configuration_file_path)
-      .expect("Could not create the configuration file");
-    let wondershaper_file_string= toml::to_string(
-        wondershaper_config
-      ).expect("Could not convert the wodershaper config file to toml");
-    let wondershaper_file_bytes = wondershaper_file_string.as_bytes();
-    wondershaper_file.write_all(
-      &wondershaper_file_bytes
-    ).expect("Could not write to the wondershaper configuration file");
+      If the file could not be created, the application will panic.
+    */
+    pub fn create_cofiguration_file(
+        configuration_file_path: String,
+        wondershaper_config: &WondershaperConfigFile,
+    ) -> Vec<u8> {
+        let mut wondershaper_file =
+            File::create(configuration_file_path).expect("Could not create the configuration file");
+        let wondershaper_file_string = toml::to_string(wondershaper_config)
+            .expect("Could not convert the wodershaper config file to toml");
+        let wondershaper_file_bytes = wondershaper_file_string.as_bytes();
+        wondershaper_file
+            .write_all(&wondershaper_file_bytes)
+            .expect("Could not write to the wondershaper configuration file");
 
-    return wondershaper_file_bytes.to_vec()
-  }
+        return wondershaper_file_bytes.to_vec();
+    }
 }
 
 /**
@@ -78,61 +87,77 @@ pub struct WondershaperConfig {
     pub NOPRIOPORTSRC: Option<String>,
     pub NOPRIOPORTDST: Option<String>,
 }
+impl WondershaperConfig {
+    pub fn new(iface: String, dspeed: String, uspeed: String) -> Self {
+        WondershaperConfig {
+            IFACE: iface,
+            DSPEED: dspeed,
+            USPEED: uspeed,
+            HIPRIODST: Option::None,
+            COMMONOPTIONS: Option::None,
+            NOPRIOHOSTSRC: Option::None,
+            NOPRIOHOSTDST: Option::None,
+            NOPRIOPORTSRC: Option::None,
+            NOPRIOPORTDST: Option::None,
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct WondershaperConfigFile {
-    pub wondershaper: WondershaperConfig
+    pub wondershaper: WondershaperConfig,
 }
-
 
 #[cfg(test)]
 mod wondershaper_test {
-  use super::{WondershaperConfigFile, Wondershaper, WondershaperConfig};
-  #[test]
-  fn create_wondershaper_file_test() {
-    let wondershaper_config = WondershaperConfig {
-      IFACE: "wlan0".to_string(),
-      DSPEED: "1024".to_string(),
-      USPEED: "1024".to_string(),
-      HIPRIODST: Option::None,
-      COMMONOPTIONS: Option::None,
-      NOPRIOHOSTSRC: Option::None,
-      NOPRIOHOSTDST: Option::None,
-      NOPRIOPORTSRC: Option::None,
-      NOPRIOPORTDST: Option::None,
-    };
-    let wondershaper_config_file = WondershaperConfigFile {
-      wondershaper: wondershaper_config
-    };
+    use super::{Wondershaper, WondershaperConfig, WondershaperConfigFile};
+    #[test]
+    fn create_wondershaper_file_test() {
+        let wondershaper_config = WondershaperConfig {
+            IFACE: "wlan0".to_string(),
+            DSPEED: "1024".to_string(),
+            USPEED: "1024".to_string(),
+            HIPRIODST: Option::None,
+            COMMONOPTIONS: Option::None,
+            NOPRIOHOSTSRC: Option::None,
+            NOPRIOHOSTDST: Option::None,
+            NOPRIOPORTSRC: Option::None,
+            NOPRIOPORTDST: Option::None,
+        };
+        let wondershaper_config_file = WondershaperConfigFile {
+            wondershaper: wondershaper_config,
+        };
 
-    Wondershaper::create_cofiguration_file(
-      "config/wondershaper.conf".to_string(), 
-      &wondershaper_config_file);
-  }
-  fn create_wondershaper_file_with_real_interfaace_test() {
-    let interfaces = Wondershaper::get_network_interfaces_name();
-    let wondershaper_config = WondershaperConfig {
-      IFACE: interfaces[1].clone(),
-      DSPEED: "1024".to_string(),
-      USPEED: "1024".to_string(),
-      HIPRIODST: Option::None,
-      COMMONOPTIONS: Option::None,
-      NOPRIOHOSTSRC: Option::None,
-      NOPRIOHOSTDST: Option::None,
-      NOPRIOPORTSRC: Option::None,
-      NOPRIOPORTDST: Option::None,
-    };
-    let wondershaper_config_file = WondershaperConfigFile {
-      wondershaper: wondershaper_config
-    };
+        Wondershaper::create_cofiguration_file(
+            "config/wondershaper.conf".to_string(),
+            &wondershaper_config_file,
+        );
+    }
+    fn create_wondershaper_file_with_real_interfaace_test() {
+        let interfaces = Wondershaper::get_network_interfaces_name();
+        let wondershaper_config = WondershaperConfig {
+            IFACE: interfaces[1].clone(),
+            DSPEED: "1024".to_string(),
+            USPEED: "1024".to_string(),
+            HIPRIODST: Option::None,
+            COMMONOPTIONS: Option::None,
+            NOPRIOHOSTSRC: Option::None,
+            NOPRIOHOSTDST: Option::None,
+            NOPRIOPORTSRC: Option::None,
+            NOPRIOPORTDST: Option::None,
+        };
+        let wondershaper_config_file = WondershaperConfigFile {
+            wondershaper: wondershaper_config,
+        };
 
-    Wondershaper::create_cofiguration_file(
-      "config/wondershaper.conf".to_string(), 
-      &wondershaper_config_file);
-  }
+        Wondershaper::create_cofiguration_file(
+            "config/wondershaper.conf".to_string(),
+            &wondershaper_config_file,
+        );
+    }
 
-  #[test]
-  fn open_wondershaper_file_test() {
-    Wondershaper::new(String::from("config/wondershaper.conf"));
-  }
+    #[test]
+    fn open_wondershaper_file_test() {
+        Wondershaper::new(String::from("config/wondershaper.conf"));
+    }
 }
